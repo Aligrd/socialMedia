3ecc1955398a5514b0f7dde37c1c9f0fada6926f
ali <mr.mirmahmodi@gmail.com>
2023-11-29 09:35:48 -0600

    added adummy data flow file

---
+++ client/src/components/Dummy-Data/dummy-template.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+const Dummy = () => {
+    return (
+        <div>
+            <h1>DummyData</h1>
+            <p>Api</p>
+        </div>
+    )
+}
+
+
+
+export default Dummy
